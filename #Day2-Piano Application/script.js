71233eca42b8dced7a73a2e11b2a8a1899d037d5
Twayfrancis <tawaifrancis@gmail.com>
2024-06-02 22:49:15 +0300

    update js file by define const keyAudio

--- #Day2-Piano Application/script.js
+++ #Day2-Piano Application/script.js
@@ -4,3 +4,14 @@ const keys = document.querySelectorAll(".key");
 const whiteKeys = document.querySelectorAll(".key.white");
 
 const blackKeys = document.querySelectorAll(".key.black.");
+
+keys.forEach(key => key.addEventListener("click", handleKeyClick));
+
+function handleKeyClick() {
+    playKey(this)
+}
+
+function playKey(key) {
+    const keyAudio = document.getElementById(key.dataset.note);
+    
+}
