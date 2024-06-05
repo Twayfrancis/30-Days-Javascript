7cb6692a64b94c0bc88ee00633c1ccdf0baad710
Twayfrancis <tawaifrancis@gmail.com>
2024-06-05 22:15:06 +0300

    update js file

--- #Day3-Text to Voice/script.js
+++ #Day3-Text to Voice/script.js
@@ -1,4 +1,12 @@
 function textToAudio() {
     let msg = document.querySelector('.text').value;
     let speech = new SpeechSynthesisUtterance();
+
+    speech.lang = 'en-UK';
+    speech.text = msg;
+    speech.volume = 200;
+    speech.rate = 1;
+    speech.pitch= 1;
+
+    speechSynthesis.speak(speech);
 }
